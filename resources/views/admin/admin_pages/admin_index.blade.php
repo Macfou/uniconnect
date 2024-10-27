f4e598c016530392673abe5c82e5ef27d3876513
jjob <jsimplina.a12137293@umak.edu.ph>
2024-10-27 21:16:24 +0800

    Add admin Index

--- resources/views/admin/admin_pages/admin_index.blade.php
+++ resources/views/admin/admin_pages/admin_index.blade.php
@@ -74,7 +74,7 @@
               <div class="relative flex flex-col bg-clip-border rounded-xl bg-white text-gray-700 shadow-md overflow-hidden xl:col-span-2">
                 <div class="relative bg-clip-border rounded-xl overflow-hidden bg-transparent text-gray-700 shadow-none m-0 flex items-center justify-between p-6">
                   <div>
-                    <h6 class="block antialiased tracking-normal font-sans text-base font-semibold leading-relaxed text-blue-gray-900 mb-1">Projects</h6>
+                    <h6 class="block antialiased tracking-normal font-sans text-base font-semibold leading-relaxed text-blue-gray-900 mb-1">Events</h6>
                     <p class="antialiased font-sans text-sm leading-normal flex items-center gap-1 font-normal text-blue-gray-600">
                       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3" stroke="currentColor" aria-hidden="true" class="h-4 w-4 text-blue-500">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5"></path>
@@ -95,98 +95,28 @@
                     <thead>
                       <tr>
                         <th class="border-b border-blue-gray-50 py-3 px-6 text-left">
-                          <p class="block antialiased font-sans text-[11px] font-medium uppercase text-blue-gray-400">companies</p>
+                          <p class="block antialiased font-sans text-[11px] font-medium uppercase text-blue-gray-400">Event</p>
                         </th>
                         <th class="border-b border-blue-gray-50 py-3 px-6 text-left">
-                          <p class="block antialiased font-sans text-[11px] font-medium uppercase text-blue-gray-400">budget</p>
+                          <p class="block antialiased font-sans text-[11px] font-medium uppercase text-blue-gray-400">Attendee</p>
                         </th>
                         <th class="border-b border-blue-gray-50 py-3 px-6 text-left">
-                          <p class="block antialiased font-sans text-[11px] font-medium uppercase text-blue-gray-400">completion</p>
+                          <p class="block antialiased font-sans text-[11px] font-medium uppercase text-blue-gray-400">Success Rate</p>
                         </th>
                       </tr>
                     </thead>
                     <tbody>
+                     
+                       
+                     
                       <tr>
                         <td class="py-3 px-5 border-b border-blue-gray-50">
                           <div class="flex items-center gap-4">
-                            <p class="block antialiased font-sans text-sm leading-normal text-blue-gray-900 font-bold">Material XD Version</p>
-                          </div>
-                        </td>
-                        
-                        <td class="py-3 px-5 border-b border-blue-gray-50">
-                          <p class="block antialiased font-sans text-xs font-medium text-blue-gray-600">$14,000</p>
-                        </td>
-                        <td class="py-3 px-5 border-b border-blue-gray-50">
-                          <div class="w-10/12">
-                            <p class="antialiased font-sans mb-1 block text-xs font-medium text-blue-gray-600">60%</p>
-                            <div class="flex flex-start bg-blue-gray-50 overflow-hidden w-full rounded-sm font-sans text-xs font-medium h-1">
-                              <div class="flex justify-center items-center h-full bg-gradient-to-tr from-blue-600 to-blue-400 text-white" style="width: 60%;"></div>
-                            </div>
-                          </div>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td class="py-3 px-5 border-b border-blue-gray-50">
-                          <div class="flex items-center gap-4">
-                            <p class="block antialiased font-sans text-sm leading-normal text-blue-gray-900 font-bold">Add Progress Track</p>
+                            <p class="block antialiased font-sans text-sm leading-normal text-blue-gray-900 font-bold">Sample Event</p>
                           </div>
                         </td>
                         <td class="py-3 px-5 border-b border-blue-gray-50">
-                          <p class="block antialiased font-sans text-xs font-medium text-blue-gray-600">$3,000</p>
-                        </td>
-                        <td class="py-3 px-5 border-b border-blue-gray-50">
-                          <div class="w-10/12">
-                            <p class="antialiased font-sans mb-1 block text-xs font-medium text-blue-gray-600">10%</p>
-                            <div class="flex flex-start bg-blue-gray-50 overflow-hidden w-full rounded-sm font-sans text-xs font-medium h-1">
-                              <div class="flex justify-center items-center h-full bg-gradient-to-tr from-blue-600 to-blue-400 text-white" style="width: 10%;"></div>
-                            </div>
-                          </div>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td class="py-3 px-5 border-b border-blue-gray-50">
-                          <div class="flex items-center gap-4">
-                            <p class="block antialiased font-sans text-sm leading-normal text-blue-gray-900 font-bold">Fix Platform Errors</p>
-                          </div>
-                        </td>
-                        <td class="py-3 px-5 border-b border-blue-gray-50">
-                          <p class="block antialiased font-sans text-xs font-medium text-blue-gray-600">Not set</p>
-                        </td>
-                        <td class="py-3 px-5 border-b border-blue-gray-50">
-                          <div class="w-10/12">
-                            <p class="antialiased font-sans mb-1 block text-xs font-medium text-blue-gray-600">100%</p>
-                            <div class="flex flex-start bg-blue-gray-50 overflow-hidden w-full rounded-sm font-sans text-xs font-medium h-1">
-                              <div class="flex justify-center items-center h-full bg-gradient-to-tr from-green-600 to-green-400 text-white" style="width: 100%;"></div>
-                            </div>
-                          </div>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td class="py-3 px-5 border-b border-blue-gray-50">
-                          <div class="flex items-center gap-4">
-                            <p class="block antialiased font-sans text-sm leading-normal text-blue-gray-900 font-bold">Launch our Mobile App</p>
-                          </div>
-                        </td>
-                        <td class="py-3 px-5 border-b border-blue-gray-50">
-                          <p class="block antialiased font-sans text-xs font-medium text-blue-gray-600">$20,500</p>
-                        </td>
-                        <td class="py-3 px-5 border-b border-blue-gray-50">
-                          <div class="w-10/12">
-                            <p class="antialiased font-sans mb-1 block text-xs font-medium text-blue-gray-600">100%</p>
-                            <div class="flex flex-start bg-blue-gray-50 overflow-hidden w-full rounded-sm font-sans text-xs font-medium h-1">
-                              <div class="flex justify-center items-center h-full bg-gradient-to-tr from-green-600 to-green-400 text-white" style="width: 100%;"></div>
-                            </div>
-                          </div>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td class="py-3 px-5 border-b border-blue-gray-50">
-                          <div class="flex items-center gap-4">
-                            <p class="block antialiased font-sans text-sm leading-normal text-blue-gray-900 font-bold">Add the New Pricing Page</p>
-                          </div>
-                        </td>
-                        <td class="py-3 px-5 border-b border-blue-gray-50">
-                          <p class="block antialiased font-sans text-xs font-medium text-blue-gray-600">$500</p>
+                          <p class="block antialiased font-sans text-xs font-medium text-blue-gray-600">500</p>
                         </td>
                         <td class="py-3 px-5 border-b border-blue-gray-50">
                           <div class="w-10/12">
